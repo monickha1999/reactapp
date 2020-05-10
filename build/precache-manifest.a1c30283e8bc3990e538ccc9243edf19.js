c6ed989f07a831d0550bb7cb0b15b40d1458f526
monickha1999 <40392288+monickha1999@users.noreply.github.com>
2020-05-10 22:59:33 +0530

    Add files via upload

---
+++ build/precache-manifest.a1c30283e8bc3990e538ccc9243edf19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f06c56b154e0b0d3672422e5a579f5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621d1c8cb7b4e7bd1f0c",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "21bd0aa92f36cfe0bbc1",
+    "url": "/static/css/main.9d400f54.chunk.css"
+  },
+  {
+    "revision": "621d1c8cb7b4e7bd1f0c",
+    "url": "/static/js/2.3964cf68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3964cf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bd0aa92f36cfe0bbc1",
+    "url": "/static/js/main.35118234.chunk.js"
+  },
+  {
+    "revision": "31b6202e5740a78af56c",
+    "url": "/static/js/runtime-main.3ac41dd5.js"
+  }
+]);
